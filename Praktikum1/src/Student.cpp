d3e447c0219ec36de0eeb12727097e50cea0c6d8
roobd <my.roobd@gmail.com>
2014-10-13 14:26:26 +0200

    Created the Projekt

---
+++ Praktikum1/src/Student.cpp
@@ -0,0 +1,21 @@
+/*
+ * Student.cpp
+ *
+ *  Created on: 13.10.2014
+ *      Author: myroo_000
+ */
+
+#include "Student.h"
+
+namespace srt {
+
+Student::Student() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Student::~Student() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace srt */
---
+++ Praktikum1/src/Student.h
@@ -0,0 +1,21 @@
+/*
+ * Student.h
+ *
+ *  Created on: 13.10.2014
+ *      Author: myroo_000
+ */
+
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+namespace srt {
+
+class Student {
+public:
+	Student();
+	virtual ~Student();
+};
+
+} /* namespace srt */
+
+#endif /* STUDENT_H_ */
