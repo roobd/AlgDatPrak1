03ce55c5a0b90b544eba3dedd951625b63d0a355
roobd <my.roobd@gmail.com>
2014-10-27 15:03:58 +0100

    Template Klasse für CursorList angefangen.

---
+++ Praktikum1/src/CursorList.h
@@ -0,0 +1,39 @@
+/*
+ * CursorList.h
+ *
+ *  Created on: 27.10.2014
+ *      Author: myroo_000
+ */
+
+#ifndef CURSORLIST_H_
+#define CURSORLIST_H_
+#define NUMENTRIES 10
+template <class T> class CursorList {
+public:
+ typedef T value_type;
+ typedef ListIterator<T> iterator;
+2
+ bool empty() const;
+ int size() const;
+ T& front() const;
+ void push_front(const T &);  // add a new value to the front of a list
+ void pop_front();
+ iterator begin() const;
+ iterator end() const;
+ iterator insert(iterator itr, const T& value); // insert before itr
+ iterator erase(iterator start, iterator stop); // stop exclusive
+ iterator erase(iterator itr);   // return ++itr
+
+private:
+ typedef struct{
+	 T data;
+	 int prev;
+	 int next;
+ }zeileT;
+ zeileT list[NUMENTRIES];
+
+};
+
+
+
+#endif /* CURSORLIST_H_ */
